9f353764d9fd325b0b84da0a1ad577afbfd88747
feltom1000 <feltom1000@hotmail.com>
2022-01-21 12:54:35 -0300

    Entity con PK compuesta CompraProductos

---
+++ src/main/java/com/spring/course/persitence/entity/ComprasProducto.java
@@ -0,0 +1,50 @@
+package com.spring.course.persitence.entity;
+
+import javax.persistence.Embedded;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "compras_productos")
+public class ComprasProducto {
+    @EmbeddedId //Cuando la PK es compuesta se utiliza esto proveniente de otra clase
+    private ComprasProductoPK id;
+
+    private Integer cantidad;
+    private BigDecimal total;
+    private Boolean estado;
+
+    public ComprasProductoPK getId() {
+        return id;
+    }
+
+    public void setId(ComprasProductoPK id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
