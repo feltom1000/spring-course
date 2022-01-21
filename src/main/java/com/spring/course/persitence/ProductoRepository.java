bba3b7b4c09112c17c018bc693bc3fbb2d0a30ec
feltom1000 <feltom1000@hotmail.com>
2022-01-21 15:05:11 -0300

    QueryMethods

--- src/main/java/com/spring/course/persitence/ProductoRepository.java
+++ src/main/java/com/spring/course/persitence/ProductoRepository.java
@@ -4,11 +4,20 @@ import com.spring.course.persitence.crud.ProductoCRUDRepository;
 import com.spring.course.persitence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCRUDRepository productoCRUDRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCRUDRepository.findAll();
+    }
+
+    public List<Producto> getProductosByCategoria(int idCategoria){
+        return productoCRUDRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCRUDRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/spring/course/persitence/crud/ProductoCRUDRepository.java
+++ src/main/java/com/spring/course/persitence/crud/ProductoCRUDRepository.java
@@ -1,8 +1,21 @@
 package com.spring.course.persitence.crud;
 
 import com.spring.course.persitence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-                                        //Recibe como parametros la Tabla y el tipo de de su PK
+import java.util.List;
+import java.util.Optional;
+
+//Recibe como parametros la Tabla y el tipo de de su PK
 public interface ProductoCRUDRepository extends CrudRepository<Producto, Integer> {
+
+    //Manera nativa de Consulta SQL:
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> getByCategoria(int idCategoria);
+
+    //O una mejor practica es utilizar los QueryMethods, que por una convencion de sintaxis ya entienden que hacer:
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
