ba502e5bf2a37190debe340cccd9bac861a3ad8f
feltom1000 <feltom1000@hotmail.com>
2022-01-23 12:34:56 -0300

    @Repository/@Component - clase ProductoRepository

--- src/main/java/com/spring/course/persitence/ProductoRepository.java
+++ src/main/java/com/spring/course/persitence/ProductoRepository.java
@@ -2,10 +2,15 @@ package com.spring.course.persitence;
 
 import com.spring.course.persitence.crud.ProductoCRUDRepository;
 import com.spring.course.persitence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository //Esta clase se encarga de interactuar con la DB, porque contiene las operaciones que queremos
+            // aplicar sobre nuestras tablas. Tambien existe @Component, que es una anotation general que
+            // significa que la clase es un componente de Spring, @Repository, es especificar que clase de
+            // component es.
 public class ProductoRepository {
     private ProductoCRUDRepository productoCRUDRepository;
 
@@ -19,5 +24,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCRUDRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCRUDRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCRUDRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCRUDRepository.deleteById(idProducto);
     }
 }
