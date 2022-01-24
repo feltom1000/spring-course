3164902ba7dd78b080d1baf7fe2b968b711ca173
feltom1000 <feltom1000@hotmail.com>
2022-01-24 13:45:23 -0300

    Exposición de nuestra API a travez de las Anotations

--- src/main/java/com/spring/course/web/controller/ProductController.java
+++ src/main/java/com/spring/course/web/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.spring.course.web.controller;
 import com.spring.course.domain.Product;
 import com.spring.course.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,23 +16,30 @@ public class ProductController {
 
     //implemento todos los metodos del service.
 
+    @GetMapping("/all") //Para obtener informacion, como parametro va el path.
     public List<Product> getAll(){
         return productService.getAll();
     }
 
-    public Optional<Product> getProduct(int productId){
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId){ //Anotation para pasar variable x path
         return productService.getProduct(productId);
     }
 
-    public Optional<List<Product>> getByCategory(int categoryId){
+    @GetMapping("/category/{categoryId}")
+    public Optional<List<Product>> getByCategory(@PathVariable("categoryId")int categoryId){
         return productService.getByCategory(categoryId);
     }
 
-    public Product save(Product product){
+    @PostMapping("/save") //porque es para enviar informacion, o guardar en este caso
+                    //@RequestBody xq el producto no va a viajar dentro del path, si no que va a ser parte
+                    // del cuerpo de la peticion
+    public Product save(@RequestBody Product product){
         return productService.save(product);
     }
 
-    public boolean delete(int productId){
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable("id") int productId){
         return productService.delete(productId);
     }
 }
