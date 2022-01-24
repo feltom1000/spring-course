1e4ec779d21a84abb87f5fbe6df36bb26819bb9a
feltom1000 <feltom1000@hotmail.com>
2022-01-24 15:50:46 -0300

    Feat: PurchaseController

--- src/main/java/com/spring/course/domain/service/PurchaseService.java
+++ src/main/java/com/spring/course/domain/service/PurchaseService.java
@@ -1,4 +1,27 @@
 package com.spring.course.domain.service;
 
+import com.spring.course.domain.Purchase;
+import com.spring.course.domain.respository.PurchaseRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PurchaseService {
+    @Autowired
+    private PurchaseRespository purchaseRepository;
+
+    public List<Purchase> getAll(){
+        return purchaseRepository.getAll();
+    }
+
+    public Optional<List<Purchase>> getByClient(String clienteId){
+        return purchaseRepository.getByClient(clienteId);
+    }
+
+    public Purchase save(Purchase purchase){
+        return purchaseRepository.save(purchase);
+    }
 }
