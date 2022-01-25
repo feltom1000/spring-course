3fc1f0b7262248f253a1e62d9b483c41b83f07b9
feltom1000 <feltom1000@hotmail.com>
2022-01-25 15:31:31 -0300

    Tokens

---
+++ src/main/java/com/spring/course/web/secutiry/JWTUtil.java
@@ -0,0 +1,19 @@
+package com.spring.course.web.secutiry;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
+public class JWTUtil {
+    private static final String KEY = "password";
+
+    public String generateToken(UserDetails userDetails){
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, KEY).compact();
+        //Estoy construyendo un JWTS(Jsonwebtoken), le seteo el user, lo obtengo, le pongo la hora de creacion
+        //le pongo la hora de vencimiento, le asigno un cifrado de algoritmo, y le asigno una key.
+    }
+}
